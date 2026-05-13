bc6055f794bf77cf284befd666102a29041b957b
macsim <112480637+macsimir@users.noreply.github.com>
2026-05-13 12:15:27 +0300

    Initial commit

---
+++ Kurs.h
@@ -0,0 +1,32 @@
+﻿
+// Kurs.h: главный файл заголовка для приложения PROJECT_NAME
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "включить pch.h до включения этого файла в PCH"
+#endif
+
+#include "resource.h"		// основные символы
+
+
+// CKursApp:
+// Сведения о реализации этого класса: Kurs.cpp
+//
+
+class CKursApp : public CWinApp
+{
+public:
+	CKursApp();
+
+// Переопределение
+public:
+	virtual BOOL InitInstance();
+
+// Реализация
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CKursApp theApp;
---
+++ KursDlg.h
@@ -0,0 +1,34 @@
+﻿
+// KursDlg.h: файл заголовка
+//
+
+#pragma once
+
+
+// Диалоговое окно CKursDlg
+class CKursDlg : public CDialogEx
+{
+// Создание
+public:
+	CKursDlg(CWnd* pParent = nullptr);	// стандартный конструктор
+
+// Данные диалогового окна
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_KURS_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// поддержка DDX/DDV
+
+
+// Реализация
+protected:
+	HICON m_hIcon;
+
+	// Созданные функции схемы сообщений
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
